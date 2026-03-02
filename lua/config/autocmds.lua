c6d3ed85f2a8c463d353c5453280f4cce7012089
winbeau <geneva4869@163.com>
2026-03-02 16:52:19 +0800

    nvim: restore C++ 4-space indent and improve new-file template cursor placement

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,7 +23,22 @@ autocmd("BufNewFile", {
     local template = vim.fn.stdpath("config") .. "/templates/cpp.cpp"
     if vim.fn.filereadable(template) == 1 then
       vim.cmd("0r " .. vim.fn.fnameescape(template))
-      vim.cmd("normal! G")
+      local target_line = nil
+      local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+      for i, line in ipairs(lines) do
+        if line:match("^%s*cin%.tie%(%s*nullptr%s*%)%s*;%s*$") then
+          target_line = i + 1
+          break
+        end
+      end
+
+      local line_count = vim.api.nvim_buf_line_count(0)
+      if target_line and target_line <= line_count then
+        vim.api.nvim_buf_set_lines(0, target_line - 1, target_line, false, { "    " })
+        vim.api.nvim_win_set_cursor(0, { target_line, 4 })
+      else
+        vim.cmd("normal! G")
+      end
     end
   end,
 })
--- templates/cpp.cpp
+++ templates/cpp.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int32_t main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-  return 0;
+    return 0;
 }
